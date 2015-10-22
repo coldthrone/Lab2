c77e3e2ce0f1f12e23dca33968993ab679db3b2d
coldthrone <chrisyu95@gmail.com>
2015-10-21 21:53:24 -0700

    Create StackList.h

---
+++ StackList.h
@@ -0,0 +1,10 @@
+#ifndef _STACKED_LIST
+#define _STACKED_LIST
+#include "StackInterface.h"
+#include "LinkedADT.h"
+#include "Node.h"
+
+
+template < class ItemType > class StackList : public LinkedADT<ItemType>  //:public StackInterface < ItemType >#pragma once
+
+#endif
